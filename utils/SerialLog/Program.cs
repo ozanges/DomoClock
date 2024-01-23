e7b8d36bfd9fc17782b7e10d1d9487b7007a7b12
ozanges <dionysosarte@hotmail.com>
2024-01-24 00:45:05 +0100

    add c# serial log for debug

---
+++ utils/SerialLog/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO.Ports;
+using System.IO;
+using System.Threading;
+
+class SerialLog
+{
+    private static string _fileName;
+
+    static void Main()
+    {
+        _fileName = $"ArduinoData_{DateTime.Now:yyyyMMdd_HHmmss}.txt";
+
+        // Configurer le port série
+        SerialPort serialPort = new SerialPort("COM6", 115200)
+        {
+            RtsEnable = true,
+            DtrEnable = true
+        };
+        serialPort.DataReceived += DataReceivedHandler;
+        serialPort.Open();
+        Console.WriteLine("Appuyez sur une touche pour fermer.");
+        Console.ReadKey();
+
+        // Fermer le port série
+        serialPort.Close();
+    }
+
+    private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+    {
+        SerialPort sp = (SerialPort)sender;
+        string indata = sp.ReadLine();
+        Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {indata}");
+        File.AppendAllText(_fileName, $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {indata}\n");
+    }
+}
