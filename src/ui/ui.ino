94ab2a2abe1392bd2c10fea3bebdceed7337fa8d
ozanges <dionysosarte@hotmail.com>
2024-01-24 00:46:37 +0100

    review communication priority with aknowledgement

--- src/ui/ui.ino
+++ src/ui/ui.ino
@@ -10,6 +10,7 @@ struct RequestData{
   char key[5];
   int  previousMillis;
   int  refreshDelay;
+  int  acknowledgeState; // 0 = must be initialized ; 1 = sent notification; 2 = acknowledged
 };
 
 struct ResponseData{
@@ -105,18 +106,24 @@ void my_touchpad_read( lv_indev_drv_t * indev_driver, lv_indev_data_t * data )
     }
 }
 
+// void TFT_SET_BL(uint8_t Value) {
+//   if (Value < 0 || Value > 100) {
+//     printf("TFT_SET_BL Error \r\n");
+//   } else {
+//     analogWrite(TFT_BL, Value * 2.55);
+//   }
+// }
+
 void setup()
 {
-    _dataList[0] = {"ep", millis(), 5000};
-    _dataList[1] = {"actp", millis(), 9000};
-    _dataList[2] = {"mitp", millis(), 9500};
-    _dataList[3] = {"matp", millis(), 15000};
-    _dataList[4] = {"as", millis(), 12000};
-    _dataList[5] = {"hp", millis(), 12500};
-    _dataList[6] = {"lp", millis(), 13000};
-    _dataList[7] = {"gp", millis(), 13500};
-    // Data data0 = {"ep", 0};
-    // Data data1 = {"ep", 1};
+    _dataList[0] = {"ep", millis(), 5 * 60 * 1000, 0}; // 5 min
+    _dataList[1] = {"actp", millis(), 2 * 60 * 1000, 0}; // 2 min
+    _dataList[2] = {"mitp", millis(), 2 * 60 * 1000, 0};
+    _dataList[3] = {"matp", millis(), 2 * 60 * 1000, 0};
+    _dataList[4] = {"as", millis(), 30 * 60 * 1000, 0}; // 30 min
+    _dataList[5] = {"hp", millis(), 5 * 60 * 1000, 0}; // 5 min
+    _dataList[6] = {"lp", millis(), 5 * 60 * 1000, 0}; // 5 min
+    _dataList[7] = {"gp", millis(), 5 * 60 * 1000, 0}; // 5 min
 
     Serial.begin( 115200 ); /* prepare for possible serial debug */
 
@@ -148,6 +155,8 @@ void setup()
 
     tft.begin();          /* TFT init */
     tft.setRotation( 0 ); /* Landscape orientation, flipped */
+    
+    // TFT_SET_BL(75); // set back light brightness
 
     lv_disp_draw_buf_init( &draw_buf, buf, NULL, screenWidth * screenHeight / 10 );
 
@@ -201,6 +210,18 @@ void calculerCouleur(uint8_t valeur, uint8_t* rouge, uint8_t* vert, uint8_t* ble
     }
 }
 
+void displayDataListValue(String title) {
+  Serial.print(title);
+  for (int i = 0; i < DATALIST_SIZE; i++) {
+    Serial.print(_dataList[i].key);
+    Serial.print(".state=");
+    Serial.print(_dataList[i].acknowledgeState);
+    Serial.print(" - ");
+  }
+
+  Serial.println();
+}
+
 void loop()
 {
   lv_timer_handler(); /* let the GUI do its work */
@@ -218,113 +239,184 @@ void loop()
     // Serial.print(response.key);
     // Serial.print(";value=");
     // Serial.println(response.value);
+
+    bool mustAknowledgeState = true;
     if (strcmp(response.key, "ep") == 0)  {
       struct timeval tv;
       tv.tv_sec = strtoll(response.value, NULL, 10);
       tv.tv_usec = 0;
       settimeofday(&tv, nullptr);
+      mustAknowledgeState = true;
+      Serial.print("epoch=");
+      Serial.println(tv.tv_sec);
     } else if (strcmp(response.key, "actp") == 0)  {
       actualTemperature = strtof(response.value, NULL);
+      Serial.print("actualTemperature=");
+      Serial.println(actualTemperature);
+      mustAknowledgeState = true;
     } else if (strcmp(response.key, "mitp") == 0)  {
       minTemperature = strtof(response.value, NULL);
+      mustAknowledgeState = true;
+      Serial.print("mitp=");
+      Serial.println(minTemperature);
     } else if (strcmp(response.key, "matp") == 0)  {
       maxTemperature = strtof(response.value, NULL);
+      mustAknowledgeState = true;
+      Serial.print("matp=");
+      Serial.println(mustAknowledgeState);
     } else if (strcmp(response.key, "as") == 0)  {
       actualStatus = response.value;
+      mustAknowledgeState = true;
+      Serial.print("as=");
+      Serial.println(actualStatus);
     } else if (strcmp(response.key, "hp") == 0)  {
       houseTodayPricePercentage = atoi(response.value);
+      mustAknowledgeState = true;
+      Serial.print("hp=");
+      Serial.println(houseTodayPricePercentage);
     } else if (strcmp(response.key, "lp") == 0)  {
       laundryTodayPricePercentage = atoi(response.value);
+      mustAknowledgeState = true;
+      Serial.print("lp=");
+      Serial.println(laundryTodayPricePercentage);
     } else if (strcmp(response.key, "gp") == 0)  {
       garageTodayPricePercentage = atoi(response.value);
+      mustAknowledgeState = true;
+      Serial.print("gp=");
+      Serial.println(garageTodayPricePercentage);
     } else {
       Serial.println("Unknown data response !");
+    }
+
+    displayDataListValue("before mustAknowledgeState : ");
+    if (mustAknowledgeState) {
+      for (int i = 0; i < DATALIST_SIZE; i++) {
+        if (strcmp(response.key, _dataList[i].key) == 0) {
+          _dataList[i].acknowledgeState = 2;
+          Serial.print("Aknowledged : ");
+          Serial.println(_dataList[i].key);
+          break;
+        }
+      }
+    }
+    displayDataListValue("after mustAknowledgeState : ");
+  }
+
+  RequestData mustAknowledgeData = {"NULL", 0, 0, 0};;
+  for (int i = 0; i < DATALIST_SIZE; i++) {
+    if (_dataList[i].acknowledgeState == 1) {
+      mustAknowledgeData = _dataList[i];
+      break;
     }
   }
 
   unsigned long currentMillis = millis();
-  for (int i = 0; i < DATALIST_SIZE; i++) {
+  if (strcmp(mustAknowledgeData.key, "NULL") != 0) {
+    if (currentMillis - _previousTXMillis >= _delayTX + 1000) {
+      _previousTXMillis = currentMillis;
+
+      displayDataListValue("before ask for aknowledgment : ");
+      mustAknowledgeData.previousMillis = currentMillis;
+      Serial.print("************ MUST AKNOWLEDGE : ");
+      Serial.println(mustAknowledgeData.key);
+      _serial.sendMessage(mustAknowledgeData.key);
+    }
+  } else {
     currentMillis = millis();
-    if (currentMillis - _dataList[i].previousMillis >= _dataList[i].refreshDelay) {
-      _dataList[i].previousMillis = currentMillis;
+    for (int i = 0; i < DATALIST_SIZE; i++) {
+      currentMillis = millis();
+      bool mustBeInitialized = _dataList[i].acknowledgeState == 0;
+      bool isTimeToRefresh = currentMillis - _dataList[i].previousMillis >= _dataList[i].refreshDelay;
+      if (mustBeInitialized || isTimeToRefresh) {
+        Serial.print("mustBeInitialized = "); 
+        Serial.print(mustBeInitialized); 
+        Serial.print("; isTimeToRefresh = "); 
+        Serial.println(isTimeToRefresh); 
+        displayDataListValue("before asking : ");    
+        _dataList[i].previousMillis = currentMillis;
 
-      Serial.print("************ ASKING FOR : ");
-      Serial.println(_dataList[i].key);
-      _serial.sendMessage(_dataList[i].key);
+        Serial.print("************ ASKING FOR : ");
+        Serial.println(_dataList[i].key);
+        _serial.sendMessage(_dataList[i].key);
+        _dataList[i].acknowledgeState = 1;
 
-      // Serial.print("i=");
-      // Serial.print(i);
-      // Serial.print(";key=");
-      // Serial.print(_dataList[i].key);
-      // Serial.print(";previousMillis=");
-      // Serial.print(_dataList[i].previousMillis);
-      // Serial.print(";refreshDelay=");
-      // Serial.println(_dataList[i].refreshDelay);
+        if (mustBeInitialized) {
+          break;
+        }
+        // Serial.print("i=");
+        // Serial.print(i);
+        // Serial.print(";key=");
+        // Serial.print(_dataList[i].key);
+        // Serial.print(";previousMillis=");
+        // Serial.print(_dataList[i].previousMillis);
+        // Serial.print(";refreshDelay=");
+        // Serial.println(_dataList[i].refreshDelay);
+      }
     }
   }
 
   currentMillis = millis();
-  if (currentMillis - _previousTXMillis >= _delayTX) {
-    _previousTXMillis = currentMillis;
-    
-    time_t now;
-    char buff[80];
+  if (currentMillis - _previousTXMillis >= _delayTX + 1000) {
+      _previousTXMillis = currentMillis;
 
-    time(&now);
-    strftime(buff, sizeof(buff), "%c", localtime(&now));
-    //Serial.println(buff);
+      time_t now;
+      char buff[80];
 
-    char dayOfWeek[4];
-    char month[4];
-    int day;
-    int hour, minute, second;
-    int year;
-    char formattedMinute[3];
+      time(&now);
+      strftime(buff, sizeof(buff), "%c", localtime(&now));
+      //Serial.println(buff);
 
-    sscanf(buff, "%s %s %d %d:%d:%d %d", dayOfWeek, month, &day, &hour, &minute, &second, &year);
-    sprintf(formattedMinute, "%02d", minute);
+      char dayOfWeek[4];
+      char month[4];
+      int day;
+      int hour, minute, second;
+      int year;
+      char formattedMinute[3];
 
-    // Affichage des résultats
-    // Serial.print("Jour de la semaine : "); Serial.println(dayOfWeek);
-    // Serial.print("Mois : "); Serial.println(month);
-    // Serial.print("Jour du mois : "); Serial.println(day);
-    // Serial.print("Heure : "); Serial.print(hour); Serial.print(":"); Serial.print(minute); Serial.print(":"); Serial.println(second);
-    // Serial.print("Année : "); Serial.println(year);
-    // Serial.print("formattedMinute : "); Serial.println(formattedMinute);
+      sscanf(buff, "%s %s %d %d:%d:%d %d", dayOfWeek, month, &day, &hour, &minute, &second, &year);
+      sprintf(formattedMinute, "%02d", minute);
 
-    int firstHour = hour / 10;
-    int secondHour = hour % 10;
-    int sec_angle = 3600 * second / 60;
-    
-    lv_label_set_text(ui_LbHour0h, String(firstHour).c_str());
-    lv_label_set_text(ui_LbHourh0, String(secondHour).c_str());
-    lv_label_set_text(ui_LbMinute00, String(formattedMinute).c_str());
-    lv_label_set_text(ui_LbDayOfWeek, dayOfWeek);
-    lv_label_set_text(ui_LbDate, String(String(day) + " " + month).c_str());
-    lv_label_set_text(ui_LbYear, String(year).c_str());
-    lv_img_set_angle(ui_ImgSeconde, sec_angle);
+      // Affichage des résultats
+      // Serial.print("Jour de la semaine : "); Serial.println(dayOfWeek);
+      // Serial.print("Mois : "); Serial.println(month);
+      // Serial.print("Jour du mois : "); Serial.println(day);
+      // Serial.print("Heure : "); Serial.print(hour); Serial.print(":"); Serial.print(minute); Serial.print(":"); Serial.println(second);
+      // Serial.print("Année : "); Serial.println(year);
+      // Serial.print("formattedMinute : "); Serial.println(formattedMinute);
 
-    String rangeTemperature = "Min: " + String(minTemperature, 0) + "° Max: " + String(maxTemperature, 0) + "°";
-    lv_label_set_text(ui_LbActualWeatherText, actualStatus.c_str());
-    lv_label_set_text(ui_LbActualTemperature, String(actualTemperature, 0).c_str());
-    lv_label_set_text(ui_LbActualRangeTemperature, rangeTemperature.c_str());
+      int firstHour = hour / 10;
+      int secondHour = hour % 10;
+      int sec_angle = 3600 * second / 60;
+      
+      lv_label_set_text(ui_LbHour0h, String(firstHour).c_str());
+      lv_label_set_text(ui_LbHourh0, String(secondHour).c_str());
+      lv_label_set_text(ui_LbMinute00, String(formattedMinute).c_str());
+      lv_label_set_text(ui_LbDayOfWeek, dayOfWeek);
+      lv_label_set_text(ui_LbDate, String(String(day) + " " + month).c_str());
+      lv_label_set_text(ui_LbYear, String(year).c_str());
+      lv_img_set_angle(ui_ImgSeconde, sec_angle);
 
-    lv_arc_set_value(ui_ArcMainElectricity, houseTodayPricePercentage);
-    lv_arc_set_value(ui_ArcSecondElectricity, laundryTodayPricePercentage);
-    lv_arc_set_value(ui_ArcThirdElectricity, garageTodayPricePercentage);
+      String rangeTemperature = "Min: " + String(minTemperature, 0) + "° Max: " + String(maxTemperature, 0) + "°";
+      lv_label_set_text(ui_LbActualWeatherText, actualStatus.c_str());
+      lv_label_set_text(ui_LbActualTemperature, String(actualTemperature, 0).c_str());
+      lv_label_set_text(ui_LbActualRangeTemperature, rangeTemperature.c_str());
 
-    uint8_t rouge, vert, bleu;
-    calculerCouleur(houseTodayPricePercentage, &rouge, &vert, &bleu);
-    lv_color_t color = lv_color_make(rouge, vert, bleu);
-    lv_obj_set_style_arc_color(ui_ArcMainElectricity, color, LV_PART_INDICATOR | LV_STATE_DEFAULT);
+      lv_arc_set_value(ui_ArcMainElectricity, houseTodayPricePercentage);
+      lv_arc_set_value(ui_ArcSecondElectricity, laundryTodayPricePercentage);
+      lv_arc_set_value(ui_ArcThirdElectricity, garageTodayPricePercentage);
 
-    calculerCouleur(laundryTodayPricePercentage, &rouge, &vert, &bleu);
-    color = lv_color_make(rouge, vert, bleu);
-    lv_obj_set_style_arc_color(ui_ArcSecondElectricity, color, LV_PART_INDICATOR | LV_STATE_DEFAULT);
-    
-    calculerCouleur(garageTodayPricePercentage, &rouge, &vert, &bleu);
-    color = lv_color_make(rouge, vert, bleu);
-    lv_obj_set_style_arc_color(ui_ArcThirdElectricity, color, LV_PART_INDICATOR | LV_STATE_DEFAULT);
+      uint8_t rouge, vert, bleu;
+      calculerCouleur(houseTodayPricePercentage, &rouge, &vert, &bleu);
+      lv_color_t color = lv_color_make(rouge, vert, bleu);
+      lv_obj_set_style_arc_color(ui_ArcMainElectricity, color, LV_PART_INDICATOR | LV_STATE_DEFAULT);
+
+      calculerCouleur(laundryTodayPricePercentage, &rouge, &vert, &bleu);
+      color = lv_color_make(rouge, vert, bleu);
+      lv_obj_set_style_arc_color(ui_ArcSecondElectricity, color, LV_PART_INDICATOR | LV_STATE_DEFAULT);
+      
+      calculerCouleur(garageTodayPricePercentage, &rouge, &vert, &bleu);
+      color = lv_color_make(rouge, vert, bleu);
+      lv_obj_set_style_arc_color(ui_ArcThirdElectricity, color, LV_PART_INDICATOR | LV_STATE_DEFAULT);
   }
 }
 
