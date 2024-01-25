9e0e51fb3032279c77a7887fcff7f92c33b412e7
ozanges <dionysosarte@hotmail.com>
2024-01-25 16:33:24 +0100

    astyle fix

--- src/core/utils.h
+++ src/core/utils.h
@@ -2,14 +2,14 @@
 
 #define MAX_NEGATIVE_DEEPSLEEP_MESSAGE 3
 
-void blink(int count) 
+void blink(int count)
 {
   for(int i = 0; i < count; i++) {
     digitalWrite(LED_BUILTIN, LOW);
     delay(125);
     digitalWrite(LED_BUILTIN, HIGH);
     delay(125);
-  }                    
+  }
 }
 
 
@@ -18,9 +18,9 @@ int _negativeDeepSleepMessageCount = 0;
 /*
  * ESP8266 : Remember to connect physically RESET pin (with D0 for Wemos)
  */
-void deepSleep(int second) 
+void deepSleep(int second)
 {
-  if (second > 0) {
+  if(second > 0) {
     delay(2000); // Minimum to give a chance to the previous action to finish (eg. data transfert over wifi)
     DPRINT(F("Going to sleep for "));
     DPRINT(second);
@@ -28,7 +28,7 @@ void deepSleep(int second)
     ESP.deepSleep(second * 1000000);
   } 
   else {
-    if (_negativeDeepSleepMessageCount++ < MAX_NEGATIVE_DEEPSLEEP_MESSAGE) {
+    if(_negativeDeepSleepMessageCount++ < MAX_NEGATIVE_DEEPSLEEP_MESSAGE) {
       DPRINTLN(F("second parameter is negative, cannot going to deepsleep."));
     }
   }
