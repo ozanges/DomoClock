2f1587ea8aff04175064aa1698c750df91a0a63b
ozanges <dionysosarte@hotmail.com>
2024-01-25 10:52:37 +0100

    get esp & rp serial logs

--- utils/SerialLog/Program.cs
+++ utils/SerialLog/Program.cs
@@ -1,36 +1,41 @@
 ﻿using System;
-using System.IO.Ports;
 using System.IO;
+using System.IO.Ports;
 using System.Threading;
 
-class SerialLog
-{
-    private static string _fileName;
-
-    static void Main()
+namespace SerialLog {
+    class Program
     {
-        _fileName = $"ArduinoData_{DateTime.Now:yyyyMMdd_HHmmss}.txt";
-
-        // Configurer le port série
-        SerialPort serialPort = new SerialPort("COM6", 115200)
+        private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
         {
-            RtsEnable = true,
-            DtrEnable = true
-        };
-        serialPort.DataReceived += DataReceivedHandler;
-        serialPort.Open();
-        Console.WriteLine("Appuyez sur une touche pour fermer.");
-        Console.ReadKey();
+            CustomSerialPort serialPort = (CustomSerialPort)sender;        
+            string data = serialPort.ReadLine();
+            Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {serialPort.ConsolePrefix} - {data}");
+            File.AppendAllText(serialPort.FileName, $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {data}\n");
+        }
 
-        // Fermer le port série
-        serialPort.Close();
-    }
+        private static CustomSerialPort InitSerialPort(string port, int baud, string fileName, string consolePrefix, bool enableRts = false, bool enableDtr = false) {
+            CustomSerialPort serialPort = new CustomSerialPort(port, baud, fileName, consolePrefix)
+            {
+                RtsEnable = enableRts,
+                DtrEnable = enableDtr
+            };
+            serialPort.DataReceived += DataReceivedHandler;
+            return serialPort;
+        }
 
-    private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
-    {
-        SerialPort sp = (SerialPort)sender;
-        string indata = sp.ReadLine();
-        Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {indata}");
-        File.AppendAllText(_fileName, $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {indata}\n");
+        private static void Main()
+        {
+            CustomSerialPort rp2040Port = InitSerialPort("COM6", 115200, $"./Data/{DateTime.Now:yyyyMMdd_HHmmss}_RP2040.txt", "[RP]", true, true);
+            CustomSerialPort esp8266Port = InitSerialPort("COM8", 115200, $"./Data/{DateTime.Now:yyyyMMdd_HHmmss}_ESP8266.txt", "[ESP]");
+            rp2040Port.Open();
+            esp8266Port.Open();
+
+            Console.WriteLine("Press any key to close.");
+            Console.ReadKey();
+
+            rp2040Port.Close();
+            esp8266Port.Close();
+        }
     }
 }
