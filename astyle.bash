9c9b0b50820a15a8c88a9ca4f05ef6846ba77066
ozanges <dionysosarte@hotmail.com>
2024-01-25 17:00:19 +0100

    add astyle for devcontainer

---
+++ astyle.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ./scripts/
+
+echo "Check style ESP8266 ... "
+python3 code-format.py "/../src/core/*.c,*.h"
+
+echo "Check style RP2040 ... "
+python3 code-format.py "/../src/ui/*.c,*.h"
